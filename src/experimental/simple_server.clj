fdc3b04fb1eb7d612537312678f00032b8bd4039
Rohit Saxena <aerohit.saxena@gmail.com>
2014-12-12 16:44:44 -0500

    Added a method for stopping the server.

--- src/experimental/simple_server.clj
+++ src/experimental/simple_server.clj
@@ -6,5 +6,12 @@
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
 
+(defonce stop-server-lambda (atom nil))
+
+(defn stop-server []
+  (when-not (nil? @stop-server-lambda)
+    (@stop-server-lambda :timeout 1000)
+    (reset! stop-server-lambda nil)))
+
 (defn -main [& args]
-  (hk/run-server app {:port 8080}))
+  (reset! stop-server-lambda (hk/run-server app {:port 8080})))
